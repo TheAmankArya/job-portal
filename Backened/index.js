e55ef0d5ddf738222af5a40d6a5428663b282789
Amank Arya <amankarya59@gmail.com>
2025-04-16 15:11:30 +0530

    cors error is fixed

--- Backened/index.js
+++ Backened/index.js
@@ -9,31 +9,30 @@ import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
 dotenv.config({});
 
-
 const app = express();
 
 //middleware
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-const corsOption={
-    origin: "http://localhost:5173",
-    credentials:true,
-}
-app.use(cors(corsOption));
+const corsOptions = {
+  origin: "*", // allow all origins
+  credentials: false, // credentials can't be true when origin is "*"
+};
+
+app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
 
 //api's
-app.use("/api/v1/user",userRoute);
-app.use("/api/v1/company",companyRoute);
-app.use("/api/v1/job",jobRoute);
-app.use("/api/v1/application",applicationRoute);
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/company", companyRoute);
+app.use("/api/v1/job", jobRoute);
+app.use("/api/v1/application", applicationRoute);
 
-
-app.listen(PORT,()=>{
-    connectDB();
-    console.log(`Server running at port ${PORT}`);
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server running at port ${PORT}`);
 });
